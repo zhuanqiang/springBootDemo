8bbcd7c1c9fa4422d8d6bccfa96739a08f2b1add
calm <2233929883@qq.com>
2018-06-21 11:16:05 +0800

    MQ 消息队列添加

--- src/main/java/com/jiedu/springbootdemo/rabbitmq/Producer.java
+++ src/main/java/com/jiedu/springbootdemo/rabbitmq/Producer.java
@@ -26,8 +26,14 @@ public class Producer {
         //创建一个通道
         Channel channel = connection.createChannel();
         //  声明一个队列        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        //queueDeclare
+        //            第一个参数表示队列名称、
+        //            第二个参数为是否持久化（true表示是，队列将在服务器重启时生存）、
+        //            第三个参数为是否是独占队列（创建者可以使用的私有队列，断开后自动删除）、
+        //            第四个参数为当所有消费者客户端连接断开时是否自动删除队列、第五个参数为队列的其他参数
         String message = "Hello RabbitMQ";
         //发送消息到队列中
+        //basicPublish第一个参数为交换机名称、第二个参数为队列映射的路由key、第三个参数为消息的其他属性、第四个参数为发送信息的主体
         channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
         System.out.println("Producer Send +'" + message + "'");
         //关闭通道和连接
